f8d32e6aefc7de2c1bb0e29d3b89b535f3b7d252
Joban Mendpara <jobanmendpara@gmail.com>
2023-11-30 08:35:06 -0500

    lspsaga

---
+++ lua/plugins/lspsaga.lua
@@ -0,0 +1,13 @@
+local M = {
+  'nvimdev/lspsaga.nvim',
+  event = "VeryLazy",
+  config = function()
+    require('lspsaga').setup({})
+  end,
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter', -- optional
+    'nvim-tree/nvim-web-devicons',     -- optional
+  }
+}
+
+return M
